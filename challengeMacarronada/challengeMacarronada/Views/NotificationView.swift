ee3545ace691f044abb29f8bfd26bb3bfec5b37b
Lorena Vilaça <lcjbv2@cin.ufpe.br>
2023-06-01 16:56:02 -0300

    feature(notification): refatoring code

--- challengeMacarronada/challengeMacarronada/Views/NotificationView.swift
+++ challengeMacarronada/challengeMacarronada/Views/NotificationView.swift
@@ -3,8 +3,8 @@ import UserNotifications
 
 
 struct NotificationView: View {
-    
-    let notificationManager: NotificationManager = NotificationManager.shared
+    @EnvironmentObject var notificationManager: NotificationManager
+
     var body: some View {
         VStack {
             VStack {
@@ -17,48 +17,19 @@ struct NotificationView: View {
                 .background(Color.blue)
                 .cornerRadius(10.0)
                 .padding()
-                Button("Add Notifications For Morning") {
-                    //Unique date components
-                    var dateComponents = DateComponents()
-                    dateComponents.hour = 15
-                    dateComponents.minute = 47
-                    //Reusable method
-//                    self.notificationManager.scheduleTriggerNotification(localNotification: LocalNotification)
-                }
-                .padding()
+
                 Button("Add Notifications For Middle Of The Day") {
                     
                     let localNotification = LocalNotification(identifier: UUID().uuidString,
                                                               title: "teste de notificação",
                                                               body: "hello",
-                                                              repeats: false)
+                                                              repeats: true)
                     
                     self.notificationManager.scheduleTriggerNotification(localNotification: localNotification)
                     
-//                    for hour in 8...24 {
-//
-//                        var dateComponents = DateComponents()
-//                        dateComponents.hour = hour
-//                        dateComponents.minute = 0
-//
-//                        self.notificationManager.scheduleTriggerNotification(title: "Middle Of The Day", body: "Did you have your daily run?", categoryIdentifier: "reminder", dateComponents: dateComponents, repeats: false)
-//
-//                    }
-
-                    //Reusable method
 
                     
                 }
-                .padding()
-                Button("Add Notifications For Night") {
-                    var dateComponents = DateComponents()
-                    dateComponents.hour = 15
-                    dateComponents.minute = 49
-                    //Reusable method
-//                    self.notificationManager.scheduleTriggerNotification(title: "Night Time", body: "Time to sleep", categoryIdentifier: "reminder", dateComponents: dateComponents, repeats: false)
-                    
-                }
-                .foregroundColor(.blue)
                 .padding()
                 
                 Button("Print Notifications") {
